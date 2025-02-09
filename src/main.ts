fb49c61bd45296e22f08e0940fdda20daee16704
Creez <creez.pw@gmail.com>
2025-02-09 12:19:53 +0100

    add typeorm dependency, mysql driver

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger, Logger } from '@nestjs/common';
 import { AppConfigService } from './config/app-config.service';
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
