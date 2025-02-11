2e628c9899a215bc0aa38e4d4656a1c2d7eea3f5
Creez <creez.pw@gmail.com>
2025-02-11 09:50:15 +0100

    add jwt expiresIn constant, add contants logging

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -1,1 +1,17 @@
-export const jwtConstants = { secret: process.env.JWT_SECRET };
+import { ConsoleLogger, Logger, LoggerService } from '@nestjs/common';
+
+const logger: LoggerService = new ConsoleLogger('Auth-Constants', { prefix: 'PW-Panel' });
+
+export const jwtConstants = { secret: process.env.JWT_SECRET, expiresIn: process.env.JWT_EXPIRES_IN };
+
+if (jwtConstants.secret) {
+  logger.log(`The JWT secret has been sucessfully initialized!`);
+} else {
+  logger.error(`An error occurred while initializing the 'JWT_SECRET' environment variable!`);
+}
+
+if (jwtConstants.expiresIn) {
+  logger.log(`The JWT expiration variable has been sucessfully initialized!`);
+} else {
+  logger.error(`An error occurred while initializing the 'JWT_EXPIRES_IN' environment variable!`);
+}
